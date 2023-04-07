9ec37078c6877aa7fc386a96c5a13fa956dbf3d6
王佳文 <128833292+jiawen-official@users.noreply.github.com>
2023-04-07 09:49:51 +0800

    fix: 优化出现动画样式

--- src/components/jw-animation/index.vue
+++ src/components/jw-animation/index.vue
@@ -1,4 +1,3 @@
-
 <script lang="ts">
 export default {
   name: 'jwAnimation'
@@ -28,93 +27,97 @@ const animationTypeList = [
   {
     titel: '引起注意效果',
     TypeList: attention,
-    fillMode: 'backwards'
-  },
-  {
-    titel: '弹跳出现效果',
-    TypeList: dapAppear,
-    fillMode: 'backwards'
-  },
-  {
-    titel: '弹跳消失效果',
-    TypeList: dapVanish,
-    fillMode: 'forwards'
-  },
-  {
-    titel: '淡入淡出 出现效果',
-    TypeList: fadAppear,
-    fillMode: 'backwards'
-  },
-  {
-    titel: '淡入淡出 消失效果',
-    TypeList: fadVanish,
-    fillMode: 'forwards'
+    fillMode: 'attention'
   },
   {
     titel: '翻转效果',
     TypeList: overturn,
-    fillMode: 'backwards'
-  },
-  {
-    titel: '急速出现效果',
-    TypeList: quicknessAppear,
-    fillMode: 'backwards'
-  },
-  {
-    titel: '急速消失效果',
-    TypeList: quicknessVanish,
-    fillMode: 'forwards'
+    fillMode: 'attention'
   },
   {
     titel: '旋转效果',
     TypeList: rotate,
-    fillMode: 'backwards'
-  },
-  {
-    titel: '旋转出现效果',
-    TypeList: rotateAppear,
-    fillMode: 'backwards'
-  },
-  {
-    titel: '旋转消失效果',
-    TypeList: rotateVanish,
-    fillMode: 'forwards'
+    fillMode: 'attention'
   },
   {
     titel: '特殊效果',
     TypeList: special,
-    fillMode: 'backwards'
+    fillMode: 'attention'
   },
+  {
+    titel: '弹跳出现效果',
+    TypeList: dapAppear,
+    fillMode: 'appear'
+  },
+  {
+    titel: '弹跳消失效果',
+    TypeList: dapVanish,
+    fillMode: 'vanish'
+  },
+  {
+    titel: '淡入淡出 出现效果',
+    TypeList: fadAppear,
+    fillMode: 'appear'
+  },
+  {
+    titel: '淡入淡出 消失效果',
+    TypeList: fadVanish,
+    fillMode: 'vanish'
+  },
+
+  {
+    titel: '急速出现效果',
+    TypeList: quicknessAppear,
+    fillMode: 'appear'
+  },
+  {
+    titel: '急速消失效果',
+    TypeList: quicknessVanish,
+    fillMode: 'vanish'
+  },
+  {
+    titel: '旋转出现效果',
+    TypeList: rotateAppear,
+    fillMode: 'appear'
+  },
+  {
+    titel: '旋转消失效果',
+    TypeList: rotateVanish,
+    fillMode: 'vanish'
+  },
+
   {
     titel: '放大缩小出现效果',
     TypeList: zoomAppear,
-    fillMode: 'backwards'
+    fillMode: 'appear'
   },
   {
     titel: '放大缩小消失效果',
     TypeList: zoomVanish,
-    fillMode: 'forwards'
+    fillMode: 'vanish'
   },
   {
     titel: '滑动出现效果',
     TypeList: slideAppear,
-    fillMode: 'backwards'
+    fillMode: 'appear'
   },
   {
     titel: '滑动消失效果',
     TypeList: slideVanish,
-    fillMode: 'forwards'
+    fillMode: 'vanish'
   }
 ]
-const setAnimation = (animationType: string, fillMode: string = 'backwards') => {
+const setAnimation = (animationType: string, fillMode: string = 'attention') => {
   // 获取HTML元素
   const element = document.querySelector(`#${animationType}-ani`) as HTMLDivElement
+  // 若动画类型为出现，则移除透明类名（让元素显示）
+  if (fillMode === 'appear') element.classList.remove('lucency');
 
   // 添加动画类名来触发动画效果
   element.classList.add(animationType);
 
-  // 若动画类型为消失，则显示最后一帧
-  // if (fillMode === 'forwards') return
+  // 若动画类型为出现或消失，则显示最后一帧
+  if (fillMode === 'appear' || fillMode === 'vanish') return
 
   // 监听动画结束事件，当动画结束时执行回调函数
   element.addEventListener('animationend', function animationEndCallback() {
@@ -133,8 +136,8 @@ const setAnimation = (animationType: string, fillMode: string = 'backwards') => 
       <p class="title">{{ item.titel }}</p>
       <div class="collection">
         <div class="item-body" v-for="(childItem, childIndex) in item.TypeList" :key="childIndex">
-          <div :id="`${childItem}-ani`" class="animated" :style="`background-image: ${background[childIndex]}`"
-            @click="setAnimation(childItem, item.fillMode)"></div>
+          <div :id="`${childItem}-ani`" class="animated" :class="{ lucency: item.fillMode === 'appear' }"
+            :style="`background-image: ${background[childIndex]}`" @click="setAnimation(childItem, item.fillMode)"></div>
           <span class="name">{{ childItem }}</span>
         </div>
       </div>
@@ -172,6 +175,10 @@ const setAnimation = (animationType: string, fillMode: string = 'backwards') => 
       width: 120px;
       height: 70px;
       border-radius: 8px;
+
+      &.lucency {
+        opacity: 0;
+      }
     }
 
     .name {
